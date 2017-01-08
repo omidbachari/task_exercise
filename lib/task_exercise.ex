2aff20ad6e608ae084fc3ed4b90e65003a1439f2
Omid Bachari <omid.bachari@launchacademy.com>
2017-01-08 08:46:05 -0500

    initial commit

---
+++ lib/task_exercise.ex
@@ -0,0 +1,18 @@
+defmodule TaskExercise do
+  @moduledoc """
+  Documentation for TaskExercise.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TaskExercise.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/task_exercise_test.exs
@@ -0,0 +1,8 @@
+defmodule TaskExerciseTest do
+  use ExUnit.Case
+  doctest TaskExercise
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
