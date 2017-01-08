560a070596abd1109a801c4ec3afd4b58b280cc6
Omid Bachari <omid.bachari@launchacademy.com>
2017-01-08 12:27:07 -0500

    add supervisor and start it

---
+++ lib/task_exercise/application.ex
@@ -0,0 +1,18 @@
+defmodule TaskExercise.Application do
+  @moduledoc """
+  The supervisor for this application
+  """
+
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      supervisor(Task.Supervisor, [[name: TaskExercise.DoJob, restart: :transient]])
+    ]
+
+    opts = [strategy: :one_for_one, name: TaskExercise.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
